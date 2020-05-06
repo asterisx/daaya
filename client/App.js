116e36b7b47907f927b2e008144248e33c3275e6
Ashwin Singh <ashwinsinghgr8@gmail.com>
2020-05-06 16:22:42 +0530

    DAAYA-17 Removed react-native-navigation and added react-navigation

--- client/App.js
+++ client/App.js
@@ -5,26 +5,25 @@
  * @format
  * @flow
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import {Home} from './screens';
 import {Provider} from 'react-redux';
 import store from './store';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App: () => React$Node = () => {
   return (
-    <Provider store={store}>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Home />
-      </SafeAreaView>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <Home />
+        </SafeAreaView>
+      </Provider>
+    </NavigationContainer>
   );
 };
-
 
 export default App;
