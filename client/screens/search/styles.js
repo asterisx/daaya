569edf01cc44a2f50a11a85f4febd0a6dc081d77
Ashwin Singh <ashwinsinghgr8@gmail.com>
2020-05-10 15:10:09 +0530

    Minor linting

--- client/screens/search/styles.js
+++ client/screens/search/styles.js
@@ -1,9 +1,9 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    wrapper: {
-        width: '100%',
-        height: '100%',
-        flexDirection: 'column',
-    },
+  wrapper: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+  },
 });
